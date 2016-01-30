5e0e1a79e5f023a2330cd8620d2ef9a0946edef3
gekkoukisi <gekkoukisi@gmail.com>
2016-01-30 15:01:46 +0900

    add test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,8 +3,11 @@ require 'item.rb'
 RSpec.describe(Item) do
   it do
     item = Item.new( "apple", 100)
+    item2 = Item.new( "banana", 50)
 
     expect(item.name).to eq("apple")
     expect(item.price).to eq(100)
+    expect(item2.name).to eq("banana")
+    expect(item2.price).to eq(50)
   end
 end
